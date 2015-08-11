62cbf6ca184f37b73317621a40c7b833789dedf7
geeky_sh <aash@targetingmantra.com>
2015-08-11 19:31:36 +0530

    refactoring discount logic

--- services/net_payable_service.rb
+++ services/net_payable_service.rb
@@ -1,9 +1,9 @@
 class NetPayableService
-  DIS_EMPLOYEE = Proc.new {|user, bill| bill.laundry? ? bill.amount : bill.amount - (0.2*bill.amount).round(2)}
-  NEW_USER = Proc.new {|user, bill| bill.laundry? ? bill.amount : bill.amount - (0.1*bill.amount).round(2)}
-  YEAR_OLD = Proc.new {|user, bill| bill.laundry? ? bill.amount : bill.amount - (0.05*bill.amount).round(2)}
-  GENERAL_DIS = Proc.new {|user, bill| bill.laundry? ? bill.amount : bill.amount - ((bill.amount/100).floor*5)}
-  NO_DIS = Proc.new {|user, bill| 0}
+  DIS_EMPLOYEE = Proc.new {|user, bill| bill.amount - (0.2*bill.amount).round(2)}
+  NEW_USER = Proc.new {|user, bill| bill.amount - (0.1*bill.amount).round(2)}
+  YEAR_OLD = Proc.new {|user, bill| bill.amount - (0.05*bill.amount).round(2)}
+  GENERAL_DIS = Proc.new {|user, bill| bill.amount - ((bill.amount/100).floor*5)}
+  NO_DIS = Proc.new {|user, bill| bill.amount}
 
   attr_accessor :user, :bill
 
@@ -13,13 +13,15 @@ class NetPayableService
   end
 
   def discounted_price
-    dp = if user.is_employee == true
+    dp = if bill.laundry?
+          NO_DIS
+        elsif user.is_employee == true
           DIS_EMPLOYEE
         elsif user.new_record == true
           NEW_USER
         elsif user.created_at < Time.now - (365*24*60*60)
           YEAR_OLD
-        elsif bill.amount > 100
+        elsif bill.amount >= 100
           GENERAL_DIS
         else
           NO_DIS
